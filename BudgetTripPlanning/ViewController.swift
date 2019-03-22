9ad65a9c2223509ada11f196bd62d06e296ba8d5
iostrends <afteryou725@outlook.com>
2019-03-22 15:14:47 +0100

    Initial commit

---
+++ BudgetTripPlanning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BudgetTripPlanning
+//
+//  Created by Alexis SANCHEZ01 (01) on 22/03/2019.
+//  Copyright © 2019 Alexis SANCHEZ01 (01). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
