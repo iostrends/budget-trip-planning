60adb145aa0c3dd5efba04877bd2024223dbcda3
iostrends <afteryou725@outlook.com>
2019-03-26 14:54:14 +0100

    Ajout Personne View Added

---
+++ Controllers/personne/PersonneAjoutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PersonneAjoutViewController.swift
+//  BudgetTripPlanning
+//
+//  Created by Alexis LUANGPRASEUTH on 26/03/2019.
+//  Copyright © 2019 Alexis SANCHEZ01 (01). All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PersonneAjoutViewController: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var nomPersonne: UITextField!
+    @IBOutlet weak var dateArrivee: UIDatePicker!
+    @IBOutlet weak var dateDepart: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
