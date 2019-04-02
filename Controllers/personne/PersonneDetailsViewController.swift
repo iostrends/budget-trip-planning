190ed904e5a8fa99ba07c0e182fabe57b80b3f14
iostrends <afteryou725@outlook.com>
2019-04-02 19:13:49 +0200

    Personne Details added

---
+++ Controllers/personne/PersonneDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PersonneDetailsViewController.swift
+//  BudgetTripPlanning
+//
+//  Created by Alexis LUANGPRASEUTH on 02/04/2019.
+//  Copyright © 2019 Alexis SANCHEZ01 (01). All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PersonneDetailsViewController: UIViewController, UINavigationControllerDelegate {
+    
+    var personne : Personne!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
