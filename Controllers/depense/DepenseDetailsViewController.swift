46773cfcfc386dce581ca5926bf43e2f870ff1fe
iostrends <afteryou725@outlook.com>
2019-04-02 19:38:52 +0200

    Depense Details added

---
+++ Controllers/depense/DepenseDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DepenseDetailsViewController.swift
+//  BudgetTripPlanning
+//
+//  Created by Alexis LUANGPRASEUTH on 02/04/2019.
+//  Copyright © 2019 Alexis SANCHEZ01 (01). All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DepenseDetailsViewController: UIViewController, UINavigationControllerDelegate {
+    
+    var depense : Depense!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
