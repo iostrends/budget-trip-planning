301e63df97e1adacd1c7c9dde970464c62e33ec5
iostrends <afteryou725@outlook.com>
2019-04-03 16:33:33 +0200

    le push

---
+++ Controllers/equilibre/EquilibreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EquilibreViewController.swift
+//  BudgetTripPlanning
+//
+//  Created by Alexis LUANGPRASEUTH on 03/04/2019.
+//  Copyright © 2019 Alexis SANCHEZ01 (01). All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EquilibreViewController: UIViewController, UINavigationControllerDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
